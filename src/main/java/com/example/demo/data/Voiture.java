752a834b8d159aaa0b9ff09e36cc18c39bb8a19b
NeilleMouton <99392012+NeilleMouton@users.noreply.github.com>
2023-04-16 17:16:49 +0200

    Update Voiture.java

--- src/main/java/com/example/demo/data/Voiture.java
+++ src/main/java/com/example/demo/data/Voiture.java
@@ -1,54 +1,48 @@
 package com.example.demo.data;
 
-import net.bytebuddy.dynamic.loading.InjectionClassLoader;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 @Entity
 public class Voiture {
+    int id;
+    String marque;
+    double prix;
 
-    private String marque;
-    private double prix;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    public int getId() {
+        return id;
+    }
 
-    public Voiture(String m, double p, int id){
-        this.marque=m;
-        this.prix=p;
-        this.id=id;
-    }
-    public Voiture(String m, double p){
-        this.marque=m;
-        this.prix=p;
-    }
-    public Voiture(){
-        this.marque="";
-        this.prix=0;
-        this.id=0;
-    }
-    public void setMarque(String marque) {
-        this.marque = marque;
-    }
-    public void setPrix(double prix) {
-        if(prix < 0){
-            return;
-        }
-        this.prix = prix;
-    }
     public void setId(int id) {
         this.id = id;
     }
 
-    public int getId() {
-        return this.id;
-    }
-    public double getPrix() {
-        return this.prix;
-    }
     public String getMarque() {
-        return this.marque;
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    @Override
+    public String toString() {
+        return "Voiture{" +
+                "id=" + id +
+                ", marque='" + marque + ''' +
+        ", prix=" + prix +
+                '}';
     }
 }
